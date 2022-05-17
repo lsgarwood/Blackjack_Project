81efe6dcfc20b0889b47a9b8a80b9d3ec7739175
Lauren <ls_garwood@yahoo.co.uk>
2022-05-17 14:59:49 +0100

    LG: First push

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 
 const saveElement = document.getElementById("draw-button");
-let header = document.querySelector("h1");
+const resetElement = document.querySelector("#reset-button");
+const foldElement = document.querySelector("#fold-button");
+const startElement = document.querySelector("#start-button");
+
+let displayGame = document.getElementById("display")
+let dataElement = document.getElementById("data");
+let array = [];
 
 function render() {
-   let value = Math.floor((Math.random() * 13) + 1);
-   console.log(value);
-   header.innerText = value;
-   
-   
+
+    let value = Math.floor((Math.random() * 13) + 1);
+    console.log(value);
 
     var newRow = document.createElement("tr");
     var newCell = document.createElement("td");
     newCell.innerHTML = value;
     newRow.append(newCell);
     document.getElementById("rows").appendChild(newRow);
+
+    array.push(value);
 }
 
+function sum() {
+    let sum = 0;
 
-console.log(inputElement);
+    for (let n = 0; n < array.length; n++) 
+    sum+= array[n];
+    console.log(sum);
+    dataElement.innerHTML = sum;
+}
+
+function refresh(){
+    window.location.reload();
+}
+
+function fold() {
+
+}
+
+function startGame() {
+
+}
+
 
 /*blackjack logic
 when you click the draw button it displays the card/number you drawed
